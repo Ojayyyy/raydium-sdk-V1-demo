a99f8ab404ab71e7bad365ac102faa9ea14bd16a
Ojayy <168037888+Ojayyyy@users.noreply.github.com>
2024-06-29 08:03:09 +0100

    Run

--- config.ts
+++ config.ts
@@ -17,14 +17,13 @@ import {
 export const rpcUrl: string = 'https://xxx.xxx.xxx/'
 export const rpcToken: string | undefined = undefined
 
-export const wallet = Keypair.fromSecretKey(Buffer.from('<YOUR_WALLET_SECRET_KEY>'))
+export const wallet = Keypair.fromSecretKey(Buffer.from('<Ekjs3DcFA1rExNNQbHRy4dUzWzeLLRAoMiyfTFA9M8DiWGtui4SEhciXSTUTmioZ7AQwzF51c3W1YVieCFraiNg>'))
 
-export const connection = new Connection('<YOUR_RPC_URL>');
+export const connection = new Connection('<JsonRpcAccountsService>');
 
 export const PROGRAMIDS = MAINNET_PROGRAM_ID;
 
 export const ENDPOINT = _ENDPOINT;
-
 export const RAYDIUM_MAINNET_API = RAYDIUM_MAINNET;
 
 export const makeTxVersion = TxVersion.V0; // LEGACY
